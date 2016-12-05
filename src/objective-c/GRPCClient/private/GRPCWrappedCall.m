cosmoskitten/grpc

GPR Buffers need to be destroyed, not directly freed, otherwise it leaks reference counted backing slices.

src/objective-c/GRPCClient/private/GRPCWrappedCall.m
@@ -112,7 +112,7 @@
 }
 
 - (void)dealloc {
-  gpr_free(_op.data.send_message);
+  grpc_slice_buffer_destroy(_op.data.send_message);
 }
 
 @end

redcar1995/shopify-grpc

src/objective-c/GRPCClient/private/GRPCWrappedCall.m
@@ -112,7 +112,7 @@
 }
 
 - (void)dealloc {
-  gpr_free(_op.data.send_message);
+  grpc_byte_buffer_destroy(_op.data.send_message);
 }
 
 @end