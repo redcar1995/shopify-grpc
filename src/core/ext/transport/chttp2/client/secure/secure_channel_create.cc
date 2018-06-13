grpc/grpc

Reviewer feedback

src/core/ext/transport/chttp2/client/secure/secure_channel_create.cc
@@ -64,9 +64,8 @@ static grpc_subchannel_args* get_secure_naming_subchannel_args(
     return nullptr;
   }
   // To which address are we connecting? By default, use the server URI.
-  const grpc_arg* server_uri_arg =
-      grpc_channel_args_find(args->args, GRPC_ARG_SERVER_URI);
-  const char* server_uri_str = grpc_channel_arg_get_string(server_uri_arg);
+  const char* server_uri_str =
+      grpc_channel_arg_find_and_get_string(args->args, GRPC_ARG_SERVER_URI);
   GPR_ASSERT(server_uri_str != nullptr);
   grpc_uri* server_uri =
       grpc_uri_parse(server_uri_str, true /* supress errors */);

src/core/lib/iomgr/tcp_client_posix.cc
@@ -66,6 +66,7 @@ typedef struct {
 static grpc_error* prepare_socket(const grpc_resolved_address* addr, int fd,
                                   const grpc_channel_args* channel_args) {
   grpc_error* err = GRPC_ERROR_NONE;
+  grpc_socket_mutator* mutator = nullptr;
 
   GPR_ASSERT(fd >= 0);
 
@@ -79,15 +80,11 @@ static grpc_error* prepare_socket(const grpc_resolved_address* addr, int fd,
   }
   err = grpc_set_socket_no_sigpipe_if_possible(fd);
   if (err != GRPC_ERROR_NONE) goto error;
-  if (channel_args) {
-    const grpc_arg* arg =
-        grpc_channel_args_find(channel_args, GRPC_ARG_SOCKET_MUTATOR);
-    grpc_socket_mutator* mutator =
-        grpc_channel_arg_get_pointer<grpc_socket_mutator>(arg);
-    if (mutator) {
-      err = grpc_set_socket_with_mutator(fd, mutator);
-      if (err != GRPC_ERROR_NONE) goto error;
-    }
+  mutator = grpc_channel_arg_find_and_get_pointer<grpc_socket_mutator>(
+      channel_args, GRPC_ARG_SOCKET_MUTATOR);
+  if (mutator != nullptr) {
+    err = grpc_set_socket_with_mutator(fd, mutator);
+    if (err != GRPC_ERROR_NONE) goto error;
   }
   goto done;
 

src/core/lib/iomgr/tcp_server_utils_posix_common.cc
@@ -150,6 +150,7 @@ grpc_error* grpc_tcp_server_prepare_socket(grpc_tcp_server* s, int fd,
                                            bool so_reuseport, int* port) {
   grpc_resolved_address sockname_temp;
   grpc_error* err = GRPC_ERROR_NONE;
+  grpc_socket_mutator* mutator = nullptr;
 
   GPR_ASSERT(fd >= 0);
 
@@ -170,16 +171,11 @@ grpc_error* grpc_tcp_server_prepare_socket(grpc_tcp_server* s, int fd,
   }
   err = grpc_set_socket_no_sigpipe_if_possible(fd);
   if (err != GRPC_ERROR_NONE) goto error;
-
-  if (s->channel_args) {
-    const grpc_arg* arg =
-        grpc_channel_args_find(s->channel_args, GRPC_ARG_SOCKET_MUTATOR);
-    grpc_socket_mutator* mutator =
-        grpc_channel_arg_get_pointer<grpc_socket_mutator>(arg);
-    if (mutator) {
-      err = grpc_set_socket_with_mutator(fd, mutator);
-      if (err != GRPC_ERROR_NONE) goto error;
-    }
+  mutator = grpc_channel_arg_find_and_get_pointer<grpc_socket_mutator>(
+      s->channel_args, GRPC_ARG_SOCKET_MUTATOR);
+  if (mutator != nullptr) {
+    err = grpc_set_socket_with_mutator(fd, mutator);
+    if (err != GRPC_ERROR_NONE) goto error;
   }
 
   if (bind(fd, reinterpret_cast<grpc_sockaddr*>(const_cast<char*>(addr->addr)),

redcar1995/shopify-grpc

src/core/ext/transport/chttp2/client/secure/secure_channel_create.cc
@@ -64,9 +64,8 @@ static grpc_subchannel_args* get_secure_naming_subchannel_args(
     return nullptr;
   }
   // To which address are we connecting? By default, use the server URI.
-  const grpc_arg* server_uri_arg =
-      grpc_channel_args_find(args->args, GRPC_ARG_SERVER_URI);
-  const char* server_uri_str = grpc_channel_arg_get_string(server_uri_arg);
+  const char* server_uri_str =
+      grpc_channel_args_get_string(args->args, GRPC_ARG_SERVER_URI);
   GPR_ASSERT(server_uri_str != nullptr);
   grpc_uri* server_uri =
       grpc_uri_parse(server_uri_str, true /* supress errors */);

src/core/lib/iomgr/tcp_client_posix.cc
@@ -66,6 +66,7 @@ typedef struct {
 static grpc_error* prepare_socket(const grpc_resolved_address* addr, int fd,
                                   const grpc_channel_args* channel_args) {
   grpc_error* err = GRPC_ERROR_NONE;
+  grpc_socket_mutator* mutator = nullptr;
 
   GPR_ASSERT(fd >= 0);
 
@@ -79,15 +80,11 @@ static grpc_error* prepare_socket(const grpc_resolved_address* addr, int fd,
   }
   err = grpc_set_socket_no_sigpipe_if_possible(fd);
   if (err != GRPC_ERROR_NONE) goto error;
-  if (channel_args) {
-    const grpc_arg* arg =
-        grpc_channel_args_find(channel_args, GRPC_ARG_SOCKET_MUTATOR);
-    grpc_socket_mutator* mutator =
-        grpc_channel_arg_get_pointer<grpc_socket_mutator>(arg);
-    if (mutator) {
-      err = grpc_set_socket_with_mutator(fd, mutator);
-      if (err != GRPC_ERROR_NONE) goto error;
-    }
+  mutator = grpc_channel_args_get_pointer<grpc_socket_mutator>(
+      channel_args, GRPC_ARG_SOCKET_MUTATOR);
+  if (mutator != nullptr) {
+    err = grpc_set_socket_with_mutator(fd, mutator);
+    if (err != GRPC_ERROR_NONE) goto error;
   }
   goto done;
 

src/core/lib/iomgr/tcp_server_utils_posix_common.cc
@@ -150,6 +150,7 @@ grpc_error* grpc_tcp_server_prepare_socket(grpc_tcp_server* s, int fd,
                                            bool so_reuseport, int* port) {
   grpc_resolved_address sockname_temp;
   grpc_error* err = GRPC_ERROR_NONE;
+  grpc_socket_mutator* mutator = nullptr;
 
   GPR_ASSERT(fd >= 0);
 
@@ -170,16 +171,11 @@ grpc_error* grpc_tcp_server_prepare_socket(grpc_tcp_server* s, int fd,
   }
   err = grpc_set_socket_no_sigpipe_if_possible(fd);
   if (err != GRPC_ERROR_NONE) goto error;
-
-  if (s->channel_args) {
-    const grpc_arg* arg =
-        grpc_channel_args_find(s->channel_args, GRPC_ARG_SOCKET_MUTATOR);
-    grpc_socket_mutator* mutator =
-        grpc_channel_arg_get_pointer<grpc_socket_mutator>(arg);
-    if (mutator) {
-      err = grpc_set_socket_with_mutator(fd, mutator);
-      if (err != GRPC_ERROR_NONE) goto error;
-    }
+  mutator = grpc_channel_args_get_pointer<grpc_socket_mutator>(
+      s->channel_args, GRPC_ARG_SOCKET_MUTATOR);
+  if (mutator != nullptr) {
+    err = grpc_set_socket_with_mutator(fd, mutator);
+    if (err != GRPC_ERROR_NONE) goto error;
   }
 
   if (bind(fd, reinterpret_cast<grpc_sockaddr*>(const_cast<char*>(addr->addr)),