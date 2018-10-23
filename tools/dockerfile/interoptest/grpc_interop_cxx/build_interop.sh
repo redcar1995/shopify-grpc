apolcyn/grpc

Add a grpclb-in-DNS interop test suite

tools/dockerfile/interoptest/grpc_interop_cxx/build_interop.sh
@@ -31,7 +31,7 @@ cd /var/local/git/grpc
 make install-certs
 
 # build C++ interop client & server
-make interop_client interop_server
+make interop_client interop_server -j8
 
 # build C++ http2 client
 make http2_client

redcar1995/shopify-grpc

tools/dockerfile/interoptest/grpc_interop_cxx/build_interop.sh
@@ -31,7 +31,7 @@ cd /var/local/git/grpc
 make install-certs
 
 # build C++ interop client & server
-make interop_client interop_server
+make interop_client interop_server -j2
 
 # build C++ http2 client
 make http2_client